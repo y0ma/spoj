5a06f106ac9fd574984ba4ed18703138c80dfa53
Anton <y0ma97@gmail.com>
2009-04-07 02:56:58 +0400

    add comments

--- divsum/divsum.c
+++ divsum/divsum.c
@@ -1,20 +1,29 @@
+/*
+	74. Divisor Summation
+	Problem code: DIVSUM
+	
+	Link: https://www.spoj.pl/problems/DIVSUM/
+*/
+
 #include <stdio.h>
 #include <math.h>
 
 int main(void) {
-	int i, testcases, n, sq;
+	int testcases, n;
+	int i, sq;
 	long long int sum;
 	
 	scanf("%d", &testcases);
 	
 	while(testcases--) {
 		scanf("%d", &n);
+		
 		sum = (n == 1) ? 0 : 1;
 		
 		sq = floor(sqrt((double) n));
 		
 		for(i = 2; i <= sq; ++i) {
-			if(n%i == 0) {
+			if(!n%i) {
 				sum += i;
 				
 				if(n/i != i) {
