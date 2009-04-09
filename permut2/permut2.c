165e4de5d3de39b2000c862d83b15872e7cf6188
Anton <y0ma97@gmail.com>
2009-04-10 01:46:50 +0400

    'solve:permut2'

---
+++ permut2/permut2.c
@@ -0,0 +1,34 @@
+/*
+	379. Ambiguous Permutations
+	Problem code: PERMUT2
+	
+	Link: https://www.spoj.pl/problems/PERMUT2/
+*/
+
+#include <stdio.h>
+
+#define MAXSIZE 100001
+
+int main(void) {
+	int len, permut[MAXSIZE];
+	int i, ans;
+
+	while(13) {
+		scanf("%d", &len);
+		
+		if(!len) {
+			break;
+		}
+		
+		for(i = ans = 1; i <= len; ++i) {
+			scanf("%d", permut + i);
+			if(ans && permut[i] <= i && permut[permut[i]] != i) {
+				ans = 0;
+			}
+		}
+		
+		printf("%sambiguous\n", ans ? "" : "not ");
+	}
+	
+	return 0;
+}
